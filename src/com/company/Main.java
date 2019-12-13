4701beb1657dcc0820d8b79b6999f3c7086f9efb
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-12-13 13:11:54 +0200

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {
+        }
+        new GUI();
     }
 }
