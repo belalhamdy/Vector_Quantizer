51105a04f42f4a7d9ed2afb358277cb4c3661bc0
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-12-13 18:14:38 +0200

    Added utilities functions to load and save images

--- src/com/company/Utilities.java
+++ src/com/company/Utilities.java
@@ -1,4 +1,46 @@
 package com.company;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.io.File;
+import java.io.IOException;
+
 public class Utilities {
+    public int[][] readImageToArray(String path) throws IOException {
+        File file = new File(path);
+        BufferedImage img = ImageIO.read(file);
+
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        int[][] ret = new int[width][height];
+
+        Raster raster = img.getData();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                ret[i][j] = raster.getSample(i, j, 0);
+            }
+        }
+
+        return ret;
+    }
+    
+    // takes directory
+    public boolean saveArrayToImage(int[][] imageArray,String to,String name) throws IOException {
+        BufferedImage image = new BufferedImage(imageArray.length, imageArray[0].length, BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < imageArray.length; i++) {
+            for (int j = 0; j < imageArray[i].length; j++) {
+                image.setRGB(i, j, imageArray[i][j]);
+            }
+        }
+
+        String path = to + "/" + name +  ".png";
+        File ImageFile = new File(path);
+        ImageIO.write(image, "png", ImageFile);
+
+        return true;
+    }
 }
